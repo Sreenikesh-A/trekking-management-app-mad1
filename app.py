7e51715593f002f836d6dc13721573ae792a8383
sreenikesh <24F2002367@ds.study.iitm.ac.in>
2026-07-09 20:23:34 +0530

    login page html

--- app.py
+++ app.py
@@ -5,9 +5,23 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        return f"login for {username}"
+        return render_template("homepage.html")
     return render_template('loginp.html') 
 
-if __name__ == '__main__':
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method=='POST':
+        name=request.form['name']
+        mail=request.form['mail']
+        password=request.form['password']
+        role=request.form['role']
+        return f"{name} registered successfully as {role}"
+
+    return render_template('register.html')
+
+@app.route('/homepage')
+def homepage():
+    return render_template('homepage.html')
+if __name__ == '__main__': 
     app.run(debug=True)
  
